d72487e1eed659942ef853b661dc306cb2d90903
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-05-02 23:58:57 -0300

    NLW day 1

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,20 @@
+import { ChatTeardropDots } from "phosphor-react"
+import { Popover } from '@headlessui/react'
+
+export function Widget() {
+    return (
+        <Popover className="absolute bottom-4 right-4">
+            <Popover.Panel>Hello World</Popover.Panel>
+
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6"/> 
+
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-2"></span>
+                    Feedback
+                </span>
+            </Popover.Button>
+        </Popover>
+    )
+   
+}
