f13e491399d094ac4f1685073c312fc1b9c4979e
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-05-04 21:54:14 -0300

    nlw return day 2

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
+
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
